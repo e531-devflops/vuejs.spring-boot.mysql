ef857c22bf76af042adc7e483dd5e02652230c15
e531-devflops <e531.devflops@gmail.com>
2021-01-26 16:04:07 +0900

    build registration form

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import LoginPage from '@/views/LoginPage'
+import RegisterPage from '@/views/RegisterPage'
 
 Vue.use(Router)
 
@@ -8,6 +9,7 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    { path: '/login', name: 'LoginPage', component: LoginPage }
+    { path: '/login', name: 'LoginPage', component: LoginPage },
+    { path: '/register', name: 'RegisterPage', component: RegisterPage }
   ]
 })
